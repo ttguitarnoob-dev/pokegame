d230d69710dc4e8e7ab743f320c3a0a83ae1d213
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-05-25 16:17:15 -0500

    trying to make it open a windown now

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pygame
+import requests
 
 # Variables
-URL = 'https://pokeapi.co/api/v2/pokemon/pikachu'
+# When searching for a poke, do an api call with URL and concatenate the name at the end
+URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+poke = 'charmander'
+response = requests.get(URL + poke)
+data = response.json()
+image = data['sprites']['other']['official-artwork']['front_default']
+# print(image)
+
+# Game Window
+screen = pygame.display.set_mode((800, 480))
+
+# Background
+background = pygame.image.load('background.png')
 
 
-# Functions
+# function
 
+# Game Loop
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
