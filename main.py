d54900f65c8f184fa72ca1425ab0e6614c318dd4
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-05-25 19:29:50 -0500

    game window opens now, and more mainstream syntax I think

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import pygame
+import pygame, sys
 import requests
 
-# Variables
-# When searching for a poke, do an api call with URL and concatenate the name at the end
-URL = 'https://pokeapi.co/api/v2/pokemon/'
+# Initialize Pygame
+pygame.init()
 
+# Variables
+# When searching for a poke, do an api call with URL and concatenate the user inputted name at the end
+URL = 'https://pokeapi.co/api/v2/pokemon/'
+clock = pygame.time.Clock()
 poke = 'charmander'
 response = requests.get(URL + poke)
 data = response.json()
@@ -13,6 +16,7 @@ image = data['sprites']['other']['official-artwork']['front_default']
 
 # Game Window
 screen = pygame.display.set_mode((800, 480))
+pygame.display.set_caption("Hazel's Pokegame")
 
 # Background
 background = pygame.image.load('background.png')
@@ -23,6 +27,19 @@ background = pygame.image.load('background.png')
 # Game Loop
 
 running = True
-while running:
+while True:
+    
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    # Drawing the Screen
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+    
