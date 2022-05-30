d654906b3cce5994cd765891734b82c70c54d291
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-05-30 18:08:31 -0500

    LR player movement events assigned

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import requests
 pygame.init()
 
 # Variables
+
+# Player Movement Variables
+moving_right = False
+moving_left = False
+
 # When searching for a poke, do an api call with URL and concatenate the user inputted name at the end
 URL = 'https://pokeapi.co/api/v2/pokemon/'
 clock = pygame.time.Clock()
 poke = 'charmander'
 response = requests.get(URL + poke)
 data = response.json()
-poke_image = data['sprites']['other']['official-artwork']['front_default']
+poke_image_url = data['sprites']['other']['official-artwork']['front_default']
 # print(image)
 
 # Game Window
@@ -40,11 +45,8 @@ player_sprite.add(player)
         
 
 # Game Loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+run = True
+while run:
 
     # Drawing the Screen
     screen.fill((0, 0, 0))
@@ -52,5 +54,35 @@ while True:
     player_sprite.draw(screen)
     pygame.display.flip()
     clock.tick(60)
+
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+        # Keyboard Events
+        if event.type == pygame.KEYDOWN:
+
+            # Keyboard Press
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            if event.key == pygame.K_RIGHT:
+                moving_right = True
+            
+            # Quit Game
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+
+
+        # Keyboard Release
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            if event.key == pygame.K_RIGHT:
+                moving_right = False
+
+
+    
 
     
